7248a0a23f9d334854584903882cc63875180c19
keep <flymayday112@gmail.com>
2023-09-06 19:30:21 +0800

    alile without css

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,9 +2,18 @@ import React from 'react'
 import Edit from './components/Edit'
 import List from './components/List'
 import Item from './components/Item'
+import './index.css'
+
+const app={
+    color:'red'
+}
 
 const Home =()=>{
-    return <div>Home</div>
+    return <div style={app}>
+        <Edit />
+    <List />
+    <Item />
+    </div>
 }
 
 export default Home
